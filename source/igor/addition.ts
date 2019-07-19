0a9a3afbbee4e5157aeed539bd369e1493e9bd2c
Igor Shubovych <igor.shubovych@gmail.com>
2019-07-19 16:02:34 +0300

    Correct my bad TS knowledge

--- source/igor/addition.ts
+++ source/igor/addition.ts
@@ -1,5 +1,3 @@
-export default {
-  add(a: number, b: number): number {
-    return a + b;
-  }
-};
+const add = (a: number, b: number): number => a + b;
+
+export default add;
--- test/igor.test.ts
+++ test/igor.test.ts
@@ -1,7 +1,11 @@
-// import add from "../source/igor/addition";
+import add from "../source/igor/addition";
 import Calculator from "../source/igor/calculator";
 
 test("addition works", () => {
   let calculator = new Calculator();
   expect(calculator.add(2, 2)).toBe(4);
+});
+
+test("another addition works", () => {
+  expect(add(2, 2)).toBe(4);
 });
