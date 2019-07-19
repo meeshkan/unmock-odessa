658d86af7a8e786aae9e1c2dbd934e33ce1863f6
dmacompton <dmacompton@gmail.com>
2019-07-19 16:51:44 +0300

    Add network request

--- test/dmitry.test.ts
+++ test/dmitry.test.ts
@@ -1,5 +1,19 @@
+import * as unmock from "unmock-node";
+
 import { myAddition } from "../src/dm/addition";
+import req from "../src/dm/network-call";
+
+beforeAll(() => {
+  unmock.on();
+});
 
 test("addition works", () => {
   expect(myAddition(2, 2)).toBe(4);
+});
+
+test("network request", async () => {
+  var data = await req();
+
+  expect(Object.keys(data).length).toEqual(1);
+  expect(typeof data.hello === "string").toBeTruthy();
 });
