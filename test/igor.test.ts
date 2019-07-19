4e0dd921f50289327929fa9191556aa890fc76e3
Igor Shubovych <igor.shubovych@gmail.com>
2019-07-19 17:03:18 +0300

    tests from tutorial

--- test/igor.test.ts
+++ test/igor.test.ts
@@ -1,7 +1,28 @@
-// import add from "../source/igor/addition";
+import add from "../source/igor/addition";
 import Calculator from "../source/igor/calculator";
 
-test("addition works", () => {
-  let calculator = new Calculator();
-  expect(calculator.add(2, 2)).toBe(4);
+import * as unmock from "unmock-node";
+import axios from "axios";
+
+test("test", () => {
+  expect(true).toBe(true);
+});
+
+beforeAll(() => {
+  unmock.on();
+});
+
+test("hello endpoint returns correct JSON", async () => {
+  var res = await axios.get("https://api.unmock.io");
+  expect(Object.keys(res.data).length).toEqual(1);
+  expect(res.data.hello).toBeDefined();
+  expect(typeof res.data.hello === "string").toBeTruthy();
+});
+
+test("setting a value for endpoint", async () => {
+  unmock.states().hello({ hello: "world" });
+  var res = await axios.get("https://api.unmock.io");
+  // TODO: Fix after this bug is fixed
+  // https://github.com/unmock/unmock-js/issues/93
+  expect(res.data).toEqual({ hello: "foo" });
 });
